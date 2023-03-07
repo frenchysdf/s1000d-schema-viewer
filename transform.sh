7b16b0e4e305989b424ccba98ea00dc48e42c42e
Stéphane Dubois <stephane@stephanedubois.com>
2023-03-07 15:11:42 -0500

    Pass path to project folder as a variable

--- transform.sh
+++ transform.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-rm -rf /Users/stephane/Dev/S1000D/HTML/*
-java -cp ~/Dev/saxon/saxon-he-11.5.jar net.sf.saxon.Transform -t -s:/Users/stephane/Dev/S1000D/Data/4.1/xml_schema_flat -xsl:/Users/stephane/Dev/S1000D/XSLT/index.xsl -o:/Users/stephane/Dev/S1000D/HTML
-for old in /Users/stephane/Dev/S1000D/HTML/*.xml; do mv $old /Users/stephane/Dev/S1000D/HTML/`basename $old .xml`.html; done
+PROJECT_FOLDER_PATH=${1}
+#1-Remove all existing HTML files
+rm -rf "${PROJECT_FOLDER_PATH}"/HTML/*
+#2-Transform Schema files with Saxon and output them in the HTML folder
+java -cp "${PROJECT_FOLDER_PATH}"/saxon/saxon-he-11.5.jar net.sf.saxon.Transform -t -s:"${PROJECT_FOLDER_PATH}"/Data/4.1/xml_schema_flat -xsl:"${PROJECT_FOLDER_PATH}"/XSLT/index.xsl -o:"${PROJECT_FOLDER_PATH}"/HTML
+#3-Change file extension from XML to HTML
+for old in "${PROJECT_FOLDER_PATH}"/HTML/*.xml; do mv $old "${PROJECT_FOLDER_PATH}"/HTML/`basename $old .xml`.html; done
