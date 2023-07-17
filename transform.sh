15e7d3bdfcd88c16af351d95d84bc7706626a849
Stéphane Dubois <stephane@stephanedubois.com>
2023-07-17 17:23:25 -0400

    Add open the start page

--- transform.sh
+++ transform.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#run the stylesheet: ./transform.sh ~/Dev/S1000D ~/Dev/S1000D/Data/4.1/xml_schema_flat
+#run the stylesheet: ./transform.sh ~/Dev/S1000D ~/Dev/S1000D/Data/4.1/
 PROJECT_FOLDER_PATH=${1}
 SCHEMA_FOLDER=${2}
 #1-Remove all existing HTML files
@@ -8,3 +8,4 @@ rm -rf "${PROJECT_FOLDER_PATH}"/HTML/*
 java -cp "${PROJECT_FOLDER_PATH}"/saxon/saxon-he-11.5.jar net.sf.saxon.Transform -t -s:"${SCHEMA_FOLDER}" -xsl:"${PROJECT_FOLDER_PATH}"/XSLT/index.xsl -o:"${PROJECT_FOLDER_PATH}"/HTML schemasFolder="${SCHEMA_FOLDER}"
 #3-Change file extension from XML to HTML
 for old in "${PROJECT_FOLDER_PATH}"/HTML/*.xml; do mv $old "${PROJECT_FOLDER_PATH}"/HTML/`basename $old .xml`.html; done
+open "${PROJECT_FOLDER_PATH}"/HTML/start_page.html
