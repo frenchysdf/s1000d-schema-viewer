1e9f0d9c14084e49a418834e2cc357d8b0460234
Stéphane Dubois <3727436+frenchysdf@users.noreply.github.com>
2025-03-19 13:46:21 -0400

    Update the file to use saxon installed with homebrew and produce a better output

--- transform.sh
+++ transform.sh
@@ -1,11 +1,41 @@
 #!/bin/bash
-#run the stylesheet: ./transform.sh ~/Dev/S1000D ~/Dev/S1000D/Data/4.1/
-PROJECT_FOLDER_PATH=${1}
-SCHEMA_FOLDER=${2}
+#run the stylesheet : ./transform.sh 
+#Run the transform process with a path to the schemas folder: ./transform.sh ~/path-to-schema
+PROJECT_FOLDER_PATH=$PWD
+SCHEMAS_FOLDER=${1:-$PROJECT_FOLDER_PATH/data}
+
 #1-Remove all existing HTML files
-rm -rf "${PROJECT_FOLDER_PATH}"/HTML/*
-#2-Transform Schema files with Saxon and output them in the HTML folder | We use the java version of Saxon for now but should move to Saxon JS so we can replace this bash script with npm scripts
-java -cp "${PROJECT_FOLDER_PATH}"/saxon/saxon-he-11.5.jar net.sf.saxon.Transform -t -s:"${SCHEMA_FOLDER}" -xsl:"${PROJECT_FOLDER_PATH}"/XSLT/index.xsl -o:"${PROJECT_FOLDER_PATH}"/HTML schemasFolder="${SCHEMA_FOLDER}"
-#3-Change file extension from XML to HTML
-for old in "${PROJECT_FOLDER_PATH}"/HTML/*.xml; do mv $old "${PROJECT_FOLDER_PATH}"/HTML/`basename $old .xml`.html; done
-open "${PROJECT_FOLDER_PATH}"/HTML/start_page.html
+echo "Cleaning the dist folder..."
+find $PROJECT_FOLDER_PATH/dist/. ! -name '.gitignore' -type f -exec rm -rf {} +
+
+#macOS only
+#There is a special place in hell for .DS_Store files
+find $PROJECT_FOLDER_PATH/data/. -name '.DS_Store' -type f -exec rm -rf {}
+
+#2-Transform every schema in the schemas folder with saxon installed with homebrew and save them to the dist folder
+echo "Transforming the schemas"
+saxon -t -s:${PROJECT_FOLDER_PATH}/data -xsl:${PROJECT_FOLDER_PATH}/src/XSLT/index.xsl -o:${PROJECT_FOLDER_PATH}/dist schemasFolder=${SCHEMAS_FOLDER}
+
+#3-Move the CSS file to the dist folder
+echo "Moving the css folder to the dist folder"
+cp -r ${PROJECT_FOLDER_PATH}/src/CSS ${PROJECT_FOLDER_PATH}/dist
+
+#4-Change file extension from XML to HTML
+echo "Change file extension to .html"
+for xmlFile in "${PROJECT_FOLDER_PATH}"/dist/*.xml; do mv $xmlFile "${PROJECT_FOLDER_PATH}"/dist/`basename $xmlFile .xml`.html; done
+
+#5-Open the start page
+open "${PROJECT_FOLDER_PATH}"/dist/start_page.html
+
+#2-Transform the Schema files with Saxon downloaded from the Saxonica website 
+#echo "Transforming the schemas"
+#java -cp "${PROJECT_FOLDER_PATH}"/saxon/saxon-he-11.5.jar net.sf.saxon.Transform -t -s:"${SCHEMAS_FOLDER}" -xsl:"${PROJECT_FOLDER_PATH}"/XSLT/index.xsl -o:"${PROJECT_FOLDER_PATH}"/dist schemasFolder="${SCHEMAS_FOLDER}"
+
+#2-Transform Schema files with saxonJS
+#Caveats: saxonJS cannot iterate through a directory so we need to create a for loop in this bash file [TODO] to iterate through the schemas folder
+
+#saxonJS installed as a dev dependency 
+#npx xslt3 -s:"${SCHEMAS_FOLDER}" -xsl:"${PROJECT_FOLDER_PATH}"/XSLT/index.xsl -o:"${PROJECT_FOLDER_PATH}"/dist schemasFolder="${SCHEMAS_FOLDER}" -t
+
+#saxonJS installed globally
+# xslt3 -s:"${SCHEMAS_FOLDER}" -xsl:"${PROJECT_FOLDER_PATH}"/XSLT/index.xsl -o:"${PROJECT_FOLDER_PATH}"/dist schemasFolder="${SCHEMAS_FOLDER}" -t
