231cc1d76d0c4771379b2298038533601dba6b15
Stéphane Dubois <3727436+frenchysdf@users.noreply.github.com>
2025-07-18 09:11:30 -0400

    Add SRC_FOLDER and DIST_FOLDER variable to stay DRY

--- transform.sh
+++ transform.sh
@@ -6,37 +6,48 @@
 
 PROJECT_FOLDER_PATH=$PWD
 SCHEMAS_FOLDER=${1:-$PROJECT_FOLDER_PATH/data}
+SRC_FOLDER=$PROJECT_FOLDER_PATH/src
+DIST_FOLDER=$PROJECT_FOLDER_PATH/dist
+
+#1-Protect your neck!
+echo "Looking for the data folder and the schemas..."
+xsd_files=($(find "$SCHEMAS_FOLDER" -maxdepth 1 -type f -name "*.xsd" -print0 | xargs -0))
+
+if [ ! -d "$SCHEMAS_FOLDER" ] || [ ${#xsd_files[@]} -eq 0 ]; then 
+  echo "The data folder is missing or empty, please create this folder, add the schema files or provide an alternative path in the build script section in package.json"; 
+  exit
+fi
 
 #1-Remove all existing HTML files
 echo "Cleaning the dist folder..."
-find "$PROJECT_FOLDER_PATH"/dist/. ! -name '.gitignore' -type f -exec rm -rf {} +
-find "$PROJECT_FOLDER_PATH"/dist/. -type d  -exec rm -rf {} +
+find "$DIST_FOLDER"/. ! -name '.gitignore' -type f -exec rm -rf {} +
+find "$DIST_FOLDER"/. -type d  -exec rm -rf {} +
 
 #macOS only
 #There is a special place in hell for .DS_Store files
-find "$PROJECT_FOLDER_PATH"/data/. -name '.DS_Store' -type f -exec rm -rf {}
+find "${SCHEMAS_FOLDER}"/. -name '.DS_Store' -type f -exec rm -rf {} +
 
 #2-Transform every schema in the schemas folder with saxon installed with homebrew and save them to the dist folder
 echo "Transforming the schemas"
-saxon -t -s:"${PROJECT_FOLDER_PATH}"/data -xsl:"${PROJECT_FOLDER_PATH}"/src/XSLT/index.xsl -o:"${PROJECT_FOLDER_PATH}"/dist schemasFolder="${SCHEMAS_FOLDER}"
+saxon -t -s:"${SCHEMAS_FOLDER}" -xsl:"${SRC_FOLDER}/XSLT/index.xsl" -o:"${DIST_FOLDER}" schemasFolder="${SCHEMAS_FOLDER}"
 
 #3-Move the CSS file to the dist folder
 echo "Moving the css folder to the dist folder"
-cp -r "${PROJECT_FOLDER_PATH}/"src/CSS "${PROJECT_FOLDER_PATH}"/dist
+cp -r "${SRC_FOLDER}/CSS" "${DIST_FOLDER}"
 
 echo "Moving the javascript folder to the dist folder"
-cp -r "${PROJECT_FOLDER_PATH}"/src/JS "${PROJECT_FOLDER_PATH}"/dist
+cp -r "${SRC_FOLDER}/JS" "${DIST_FOLDER}"
 
-cp -r "${PROJECT_FOLDER_PATH}"/src/IMG/favicon.ico "${PROJECT_FOLDER_PATH}"/dist
+cp -r "${SRC_FOLDER}/IMG/favicon.ico" "${DIST_FOLDER}"
 
 #4-Change file extension from XML to HTML
 echo "Change file extension to .html"
-for xmlFile in "${PROJECT_FOLDER_PATH}"/dist/*.xml; do mv "$xmlFile" "${PROJECT_FOLDER_PATH}"/dist/`basename $xmlFile .xml`.html; done
+for xmlFile in "${DIST_FOLDER}"/*.xml; do mv "$xmlFile" "${DIST_FOLDER}"/`basename $xmlFile .xml`.html; done
 
 #5-Open the start page
 echo "Open the start page"
 # open "${PROJECT_FOLDER_PATH}"/dist/start_page.html
-cd "${PROJECT_FOLDER_PATH}"/dist && live-server --open=start_page.html
+cd "${DIST_FOLDER}" && live-server --open=start_page.html
 
 ## Keep the content below for documentation and alternative use
 
